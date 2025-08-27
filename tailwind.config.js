25ae59e0ea7f1d55f5bc0f7c0daeb3991a950b73
Pablo <pablobarcellossoares@gmail.com>
2025-08-27 16:13:21 -0300

    Create stiles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pomodoro: {
+          50: "#C36363",
+          55: "#B85B5B",
+          100: "#E04E4E",
+          200: "#A63232",
+        },
+        short: {
+          50: "#64AEBC",
+          55: "#519BA9",
+          100: "#38858A",
+          200: "#295F63",
+        },
+        long: {
+          50: "#6398C3",
+          55: "#4F86B3",
+          100: "#4D7C9E",
+          200: "#356384",
+        },
+        text: {
+          100: "#FFFFFF",
+          200: "#E0E0E0",
+        },
+        config: {
+          100: "#333333",
+          200: "#282828",
+        },
+      },
+
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
